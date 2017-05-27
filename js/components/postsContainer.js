473c29945ad05694d03fd41736f4e493087573e4
David Nimmo <dnimmo@gmail.com>
2017-05-27 17:04:00 +0100

    expand post started...

--- js/components/postsContainer.js
+++ js/components/postsContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
+import { Link } from 'react-router-dom'
 
 async function getPostData () {
   const postData = await fetch('/api/posts.json')
@@ -9,13 +10,52 @@ async function getPostData () {
   return postData
 }
 
-const LatestPost = ({ data = {}}) => (
+let selectedPost = {}
+let show = false
+
+const updateSelectedPost = ({data, display}) => {
+  console.log('updatedThingCalled')
+  selectedPost = data
+  show = display
+}
+
+const ExpandedPost = () => (
+  <div id="open" className={ show ? "overlay show" : "overlay" } >
+    <article className="page">
+      <a href="#" className="close">x</a>
+      <header>
+        <section className="ep-meta">
+          <span className="date">{selectedPost.shortdate}</span>
+          <h3>{selectedPost.title}</h3>
+          <p>{selectedPost.content}</p>
+        </section>
+      </header>
+      <section className="container info">
+        <div>
+          <h2>Listen</h2>
+          <hr></hr>
+          <h2 id="links">Links:</h2>
+          <ul>
+            <li>links</li>
+          </ul>
+          <h2 id="feed">Feed</h2>
+          <hr></hr>
+          <p>Feed link: feedLink</p>
+          <h2 id="acknowledgtements">Acknowledgtements</h2>
+          <hr></hr>
+        </div>
+      </section>
+    </article>
+  </div>
+)
+
+const LatestPost = ({ data = {} }) => (
   <li className='post latest'>
     <header>
       <h2>Latest Post</h2>
       <span className='date'>{data.shortdate}</span><br />
       <hr />
-      <a href='#open'><h3>{data.title}</h3></a>
+      <Link to='#open' onClick={() => updateSelectedPost({data, display: true})}><h3>{data.title}</h3></Link>
       <p></p>
     </header>
   </li>
@@ -23,10 +63,9 @@ const LatestPost = ({ data = {}}) => (
 
 const Post = ({ data = {} }) => (
   <li className='post'>
-    {console.log(data)}
     <header>
       <span className='date'>{data.shortdate}</span>
-      <a href='#open'><h3>{data.title}</h3></a>
+      <Link to='#open' onClick={updateSelectedPost({data, display: true})}><h3>{data.title}</h3></Link>
       <p>{data.excerpt}</p>
     </header>
   </li>
@@ -41,6 +80,7 @@ const PostsContainer = ({ data = [] }) => (
           if (x !== data[0]) return <Post data={x} />
         } ) }
       </ul>
+      <ExpandedPost />
     </section>
   </main>
 )
--- js/components/siteContainer.js
+++ js/components/siteContainer.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import Header from './header'
 import PostsContainer from './postsContainer'
 import Footer from './footer'
@@ -18,11 +23,13 @@ class App extends Component {
 
   render () {
     return (
-      <div>
-        <Header />
-        <PostsContainer data={this.state.posts}/>
-        <Footer />
-      </div>
+      <Router>
+        <div>
+          <Header />
+          <PostsContainer data={this.state.posts}/>
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
